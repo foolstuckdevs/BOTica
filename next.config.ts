bdca736dd4e526db0787d06e796e8ddc15f24f85
joe <joedaulayan@aldersgate.edu.ph>
2025-07-08 16:28:16 +0800

    add supabase domain

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
+  images: {
+    domains: ['otbrcucsdrjvlrsdihnu.supabase.co'],
+  },
 };
 
 export default nextConfig;
